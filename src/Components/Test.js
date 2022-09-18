a0ec9ae568284e4c8956c93f8c1ae3336fee6d71
DocCringol <eldarshaykhiev@gmail.com>
2022-09-18 10:59:52 +0500

    Add React App

---
+++ src/Components/Test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Transitions from './Transition'
+
+const Test = () => {
+	return (
+		<>
+			<Transitions>
+				<h3 className='mt-5' class="ab"
+					style={{ color: 'green' }}>
+					My dick as big as your mom
+				</h3>
+			</Transitions>
+		</>
+	)
+}
+
+export default Test
---
+++ src/Components/Transition.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import {motion} from 'framer-motion/dist/es/index'
+
+
+const animationConfiguration = {
+	initial: { opacity: 0 },
+	animate: { opacity: 1 },
+	exit: { opacity: 0 },
+};
+const Transitions = ({ children }) => {
+	return (
+		<motion.div
+			variants={animationConfiguration}
+			initial="initial"
+			animate="animate"
+			exit="exit"
+			transition={{ duration: 1 }}
+		>
+			{children}
+		</motion.div>
+	);
+};
+export default Transitions;
