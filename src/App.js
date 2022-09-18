5475f667197639cdc3fced909ff782fa2d3378d3
DocCringol <eldarshaykhiev@gmail.com>
2022-09-18 13:59:43 +0500

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import './Components/hovermenu.css'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
+import './Components/hovermenu.css'
 import Home from './Components/Home';
 import Test from './Components/Test';
-import {AnimatePresence, motion} from 'framer-motion/dist/framer-motion'
+import {AnimatePresence} from 'framer-motion/dist/framer-motion'
 
 
 const Animated = () => {
@@ -23,21 +23,20 @@ const Animated = () => {
 class App extends Component {
 	render() {
 		return (
-			<div className="container">
+			<div class="container">
 				<>
 					<Router>
 						<Animated />
+						<nav id="mySidenav" class="sidenav">
+							<Link to="/" id="home">Home</Link>
+							<Link to="/test" id="test">Test</Link>
+						</nav>
 					</Router>
 				</>
-				<div id="mySidenav" class="sidenav">
-					<a href="/" id="about">Home</a>
-					<a href="/test" id="blog">Test</a>
-					<a href="#" id="projects">Projects</a>
-					<a href="#" id="contact">Contact</a>
-				</div>
 			</div>
 		)
 	}
 }
+
 
 export default App
