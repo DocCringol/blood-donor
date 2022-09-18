34623943cf410a48e57ef82581c0805d18af901b
DocCringol <eldarshaykhiev@gmail.com>
2022-09-18 14:50:25 +0500

    Исправил meta description

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from './Components/Navbar'
 class App extends Component {
 	render() {
 		return (
-			<div class="container">
+			<div className="container">
 				<Navbar />
 			</div>
 		)
